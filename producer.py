9597873129dca9f2b0a6ba5c36d5e2fdb6f346b0
safrazRampersaud <safraz.rampersaud@gmail.com>
2023-02-22 01:43:38 -0500

    simplifying producer

--- producer.py
+++ producer.py
@@ -8,68 +8,65 @@ import json
 logger = logging.getLogger(__name__)
 
 
-class YouTubeApiMethods:
+def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=None):
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems",
+                            params={
+                                "key": google_api_key,
+                                "playlistId": youtube_playlist_id,
+                                "part": "contentDetails",
+                                "pageToken": page_token
+                            })
 
-    @staticmethod
-    def fetch_playlist_items_page(self, google_api_key, youtube_playlist_id, page_token=None):
-        response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems",
-                                params={
-                                    "key": google_api_key,
-                                    "playlistId": youtube_playlist_id,
-                                    "part": "contentDetails",
-                                    "pageToken": page_token
-                                })
+    payload = json.loads(response.text)
+    return payload
 
-        payload = json.loads(response.text)
-        return payload
 
-    @staticmethod
-    def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
-        payload = YouTubeApiMethods.fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token)
+def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
+    payload = fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token)
 
-        yield from payload.get("items")
+    yield from payload.get("items")
 
-        next_page_token = payload.get("nextPageToken")
+    next_page_token = payload.get("nextPageToken")
 
-        if next_page_token is not None:
-            yield from YouTubeApiMethods.fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
+    if next_page_token is not None:
+        yield from fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
 
-    @staticmethod
-    def fetch_videos_page(google_api_key, video_id, page_token=None):
-        response = requests.get("https://www.googleapis.com/youtube/v3/videos",
-                                params={
-                                    "key": google_api_key,
-                                    "id": video_id,
-                                    "part": "snippet,statistics",
-                                    "pageToken": page_token
-                                })
-        payload = json.loads(response.text)
-        return payload
 
-    @staticmethod
-    def fetch_videos(google_api_key, video_id, page_token=None):
-        payload = YouTubeApiMethods.fetch_videos_page(google_api_key, video_id, page_token)
+def fetch_videos_page(google_api_key, video_id, page_token=None):
+    response = requests.get("https://www.googleapis.com/youtube/v3/videos",
+                            params={
+                                "key": google_api_key,
+                                "id": video_id,
+                                "part": "snippet,statistics",
+                                "pageToken": page_token
+                            })
+    payload = json.loads(response.text)
+    return payload
 
-        yield from payload.get("items")
 
-        next_page_token = payload.get("nextPageToken")
+def fetch_videos(google_api_key, video_id, page_token=None):
+    payload = fetch_videos_page(google_api_key, video_id, page_token)
 
-        if next_page_token is not None:
-            yield from YouTubeApiMethods.fetch_videos(google_api_key, video_id, next_page_token)
+    yield from payload.get("items")
 
-    @staticmethod
-    def summarize_video(video):
-        return {
-            "video_id": video["id"],
-            "title": video["snippet"]["title"],
-            "views": int(video['statistics'].get("viewCount", 0)),
-            "likes": int(video['statistics'].get("likeCount", 0)),
-            "comments": int(video['statistics'].get("commentCount", 0))
-        }
+    next_page_token = payload.get("nextPageToken")
 
-    @staticmethod
-    def on_delivery(err, record):
-        pass
+    if next_page_token is not None:
+        yield from fetch_videos(google_api_key, video_id, next_page_token)
+
+
+def summarize_video(video):
+    return {
+        "video_id": video["id"],
+        "title": video["snippet"]["title"],
+        "views": int(video['statistics'].get("viewCount", 0)),
+        "likes": int(video['statistics'].get("likeCount", 0)),
+        "comments": int(video['statistics'].get("commentCount", 0))
+    }
+
+
+def on_delivery(err, record):
+    pass
 
 
 def process_producer():
@@ -80,9 +77,9 @@ def process_producer():
     bootstrap_servers = config["kafka"]["bootstrap_servers"]
 
     producer = KafkaProducer(bootstrap_servers=bootstrap_servers, value_serializer=lambda x: dumps(x).encode('utf-8'))
-    for video_item in YouTubeApiMethods.fetch_playlist_items(google_api_key, youtube_playlist_id):
+    for video_item in fetch_playlist_items(google_api_key, youtube_playlist_id):
         video_id = video_item["contentDetails"]["videoId"]
-        for video in YouTubeApiMethods.fetch_videos(google_api_key, video_id):
+        for video in fetch_videos(google_api_key, video_id):
             data = {
                     video_id: {
                         "TITLE": video["snippet"]["title"],
